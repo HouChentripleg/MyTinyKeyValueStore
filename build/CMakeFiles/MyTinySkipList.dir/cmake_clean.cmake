40b490be6785158436c77a1ead21f50ee82dcb40
HouChentripleg <tripleghc@163.com>
2022-06-14 01:55:30 +0800

    first commit

---
+++ build/CMakeFiles/MyTinySkipList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyTinySkipList.dir/main.cpp.o"
+  "MyTinySkipList.pdb"
+  "MyTinySkipList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyTinySkipList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TinySkipListLib.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "libTinySkipListLib.pdb"
+  "libTinySkipListLib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/TinySkipListLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
